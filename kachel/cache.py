dbc9a044a525c1acebd16d9c295f9124bdd05b85
Robin Hutmacher <robin.hutmacher@gmail.com>
2023-02-09 21:14:59 +0100

    Refactor cache computation
    Add multiple zoom levels. Cache maps (x, y, z) to unique index of covered tiles.

--- kachel/cache.py
+++ kachel/cache.py
@@ -1,14 +1,15 @@
 """Compute a cache file given a geojson file.
 
 A cache file is a dict mapping (x, y, z) to a boolean indicating whether
-the tile is covered by the geojson file.
+the tile in zoom level 14 is covered by the geojson file.
 """
 
+from collections import defaultdict
 import argparse
 import json
 import pickle
 
-from kachel.utils import deg2num
+from kachel.utils import deg2num, get_parent_tile
 
 
 def main():
@@ -24,17 +25,28 @@ def main():
     visited = set()
     for route in routes:
         coordinates = route["geometry"]["coordinates"]
-        for (lng1, lat1), (lng2, lat2) in zip(coordinates, coordinates[1:]):
-            for zoom in range(7, 15):
-                x1, y1 = deg2num(lat1, lng1, zoom)
-                x2, y2 = deg2num(lat2, lng2, zoom)
+        # TODO: Properly handle interpolation between tiles
+        #  For now, we assume the coordinates are close enough
+        for lng, lat in coordinates:
+            zoom = 14
+            x, y = deg2num(lat, lng, zoom)
+            visited.add((x, y, zoom))
 
-                visited.add((x1, y1, zoom))
-                visited.add((x2, y2, zoom))
-                # TODO: Add interpolation between tiles
+    cache = defaultdict(int)
+    for zoom in range(14, 9, -1):
+        for x, y, z in visited:
+            parent_x, parent_y = get_parent_tile(x, y, zoom)
+
+            n_tiles = 2 ** (14 - zoom)  # Number of level 14 tiles in this zoom level
+            # The x and y coordinates of the parent tile in level 14
+            parent_x_l14 = parent_x * n_tiles
+            parent_y_l14 = parent_y * n_tiles
+
+            idx = (x - parent_x_l14) * n_tiles + (y - parent_y_l14)
+            cache[(parent_x, parent_y, zoom)] |= 1 << idx
 
     with open(args.cache, "wb") as f:
-        pickle.dump(visited, f)
+        pickle.dump(cache, f)
 
 
 if __name__ == "__main__":
